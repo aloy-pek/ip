a3ae26cbd9fdac3cbbb93a826071993adb45e4f0
Aloysius Pek <aloysius7m@gmail.com>
2025-08-21 16:58:55 +0800

    Add List and add *.class to gitignore

--- src/main/java/Kuro.java
+++ src/main/java/Kuro.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Kuro {
@@ -13,6 +15,8 @@ public class Kuro {
                 ____________________________________________________________
                 """;
     
+    static List<String> commandList = new ArrayList<String>();
+    
     public static void main(String[] args) {
         boolean isOperating = true;
         
@@ -21,15 +25,32 @@ public class Kuro {
         while (isOperating) {
             Scanner scannerObj = new Scanner(System.in);
             String command = scannerObj.nextLine();
-            
-            if  (command.equalsIgnoreCase("bye")) {
-                isOperating = false;
-            } else {
-                System.out.printf("""
-                        ____________________________________________________________
-                        %s
-                        ____________________________________________________________
-                        %n""", command);
+
+            switch (command.toLowerCase()) {
+                case "bye":
+                    isOperating = false;
+                    break;
+                case "list":
+                    StringBuilder listString = new StringBuilder();
+                    for (int i = 0; i < commandList.size(); i++){
+                        listString.append("\n").append(i + 1).append(". ").append(commandList.get(i));
+                    }
+                    System.out.printf("""
+                            ____________________________________________________________
+                            %s
+                            ____________________________________________________________
+                            %n""", listString);
+                    break;
+                default:
+                    if (command.isEmpty()) {
+                        break;
+                    }
+                    commandList.add(command);
+                    System.out.printf("""
+                            ____________________________________________________________
+                            added: %s
+                            ____________________________________________________________
+                            %n""", command);
             }
         }
         System.out.println(goodbyeMsg);
