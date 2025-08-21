e710db7237ad1334ed813e55d0f0b01da9da1fc3
Aloysius Pek <aloysius7m@gmail.com>
2025-08-21 11:18:07 +0800

    Add Echo Feature

--- src/main/java/Kuro.java
+++ src/main/java/Kuro.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
+
 public class Kuro {
-    public static void main(String[] args) {
-        String welcomeMsg = """
+    final static String welcomeMsg = """
                 ____________________________________________________________
                  Hello! I'm Kuro
                  What can I do for you?
                 ____________________________________________________________
                 """;
-        String goodbyeMsg = """
-                 Bye. Hope to see you again soon!
+    final static String goodbyeMsg = """
+                ____________________________________________________________
+                 Sayonara! Hope to see you again soon!
                 ____________________________________________________________
                 """;
+    
+    public static void main(String[] args) {
+        boolean isOperating = true;
+        
         System.out.println(welcomeMsg);
+        
+        while (isOperating) {
+            Scanner scannerObj = new Scanner(System.in);
+            String command = scannerObj.nextLine();
+            
+            if  (command.equalsIgnoreCase("bye")) {
+                isOperating = false;
+            } else {
+                System.out.printf("""
+                        ____________________________________________________________
+                        %s
+                        ____________________________________________________________
+                        %n""", command);
+            }
+        }
         System.out.println(goodbyeMsg);
     }
 }
