0e6eb20f9e0d5fcdca9b5d8d98f44320ea4f0b2f
Aloysius Pek <aloysius7m@gmail.com>
2025-08-21 10:56:53 +0800

    Rename, Add Hello, Add Goodbye

---
+++ src/main/java/Kuro.java
@@ -0,0 +1,16 @@
+public class Kuro {
+    public static void main(String[] args) {
+        String welcomeMsg = """
+                ____________________________________________________________
+                 Hello! I'm Kuro
+                 What can I do for you?
+                ____________________________________________________________
+                """;
+        String goodbyeMsg = """
+                 Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+        System.out.println(welcomeMsg);
+        System.out.println(goodbyeMsg);
+    }
+}
