b7f81faa288178670bb9e2181e76905eb23425a3
Aloysius Pek <aloysius7m@gmail.com>
2025-09-09 10:58:01 +0800

    Separate the normalizing for windows and Linux/macOS

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+SETLOCAL ENABLEDELAYEDEXPANSION
 
 REM create bin directory if it doesn't exist
 if not exist ..\bin mkdir ..\bin
@@ -7,6 +8,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
+set SOURCES=
 for /R ..\src\main\java\kuro %%f in (*.java) do (
     echo %%f | findstr /i "\\gui\\" >nul
     if errorlevel 1 (
@@ -14,7 +16,10 @@ for /R ..\src\main\java\kuro %%f in (*.java) do (
     )
 )
 
-REM no error here, errorlevel == 0
+REM compile all collected source files into ..\bin
+if defined SOURCES (
+    javac -d ..\bin !SOURCES!
+)
 
 REM run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ..\bin kuro.chatbot.Kuro < input.txt > ACTUAL.TXT
