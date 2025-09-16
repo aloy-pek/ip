f77e7692550768756c4f9e32286e813a36c54ca5
Aloysius Pek <aloysius7m@gmail.com>
2025-09-16 12:21:44 +0800

    Add more test case and improve code quality

--- src/main/java/kuro/parser/CommandParser.java
+++ src/main/java/kuro/parser/CommandParser.java
@@ -1,10 +1,15 @@
 package kuro.parser;
 
+import static kuro.constants.ParserConstants.EVENT_END_INDICATOR;
+import static kuro.constants.ParserConstants.EVENT_START_INDICATOR;
+import static kuro.constants.ParserConstants.MINIMUM_EVENT_LENGTH;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 import kuro.constants.Messages;
+import kuro.constants.ParserConstants;
 import kuro.exceptions.KuroException;
 import kuro.tasks.Deadline;
 import kuro.tasks.Event;
@@ -43,63 +48,117 @@ public class CommandParser {
             throw new KuroException(Messages.UNREGISTERED_COMMAND);
         }
     }
+    // -------------------------------- TODOs TASK ---------------------------------------------------------------
     private Task parseTodo(String fullCommand, TaskList tasks) throws KuroException {
-        if (fullCommand.length() < 4) {
-            throw new KuroException(Messages.MISSING_TASK_DESCRIPTION);
-        }
-        String description = fullCommand.substring(fullCommand.indexOf(" ") + 1);
-        if (tasks.hasDuplicate(description)) {
-            throw new KuroException(Messages.DUPLICATE_ERROR);
-        }
+        validateTodoCommand(fullCommand);
+        String description = extractTodoDescription(fullCommand);
+        validateDuplicate(tasks, description);
         return new Todo(description);
     }
+
+    private void validateTodoCommand(String fullCommand) throws KuroException {
+        if (fullCommand.length() <= ParserConstants.MINIMUM_TODO_LENGTH) {
+            throw new KuroException(Messages.MISSING_TASK_DESCRIPTION);
+        }
+    }
+
+    private String extractTodoDescription(String fullCommand) throws KuroException {
+        String description = fullCommand.substring(fullCommand.indexOf(" ") + 1).trim();;
+        if (description.isEmpty()) {
+            throw new KuroException(Messages.MISSING_TASK_DESCRIPTION);
+        }
+        return description;
+    }
+    // -------------------------------- DEADLINE TASK ---------------------------------------------------------------
     private Task parseDeadline(String fullCommand, TaskList tasks) throws KuroException {
-        boolean isInvalidDeadlineCommand = !fullCommand.contains("/by") || fullCommand.length() < 13;
+        validateDeadlineCommand(fullCommand);
+        String description = extractDeadlineDescription(fullCommand);
+        validateDuplicate(tasks, description);
+        LocalDateTime by = parseDateTime(extractDeadlineBy(fullCommand));
+        return new Deadline(description, by);
+    }
+    private void validateDeadlineCommand(String fullCommand) throws KuroException {
+        boolean isInvalidDeadlineCommand = !fullCommand.contains(ParserConstants.DEADLINE_INDICATOR)
+                || fullCommand.length() <= ParserConstants.MINIMUM_DEADLINE_LENGTH;
         if (isInvalidDeadlineCommand) {
             throw new KuroException(Messages.INVALID_COMMAND);
         }
-        String description = fullCommand.substring(9, fullCommand.indexOf("/by")).trim();
-        String by = fullCommand.substring(fullCommand.indexOf("/by") + 4).trim();
-        if (tasks.hasDuplicate(description)) {
-            throw new KuroException(Messages.DUPLICATE_ERROR);
-        }
-        try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            LocalDateTime dateTime = LocalDateTime.parse(by, formatter);
-            return new Deadline(description, dateTime);
-        } catch (DateTimeParseException e) {
-            throw new KuroException(Messages.INVALID_DATE);
-        }
     }
+
+    private String extractDeadlineDescription(String fullCommand) throws KuroException {
+        String description = fullCommand.substring(9, fullCommand.indexOf(ParserConstants.DEADLINE_INDICATOR)).trim();
+        if (description.isEmpty()) {
+            throw new KuroException(Messages.MISSING_TASK_DESCRIPTION);
+        }
+        return description;
+    }
+
+    private String extractDeadlineBy(String fullCommand) {
+        return fullCommand.substring(fullCommand.indexOf(ParserConstants.DEADLINE_INDICATOR) + 4).trim();
+    }
+
+    // -------------------------------- EVENT TASK ---------------------------------------------------------------
     private Task parseEvent(String fullCommand, TaskList tasks) throws KuroException {
-        boolean isInvalidEventCommand = !fullCommand.contains("/from")
-                || !fullCommand.contains("/to")
-                || fullCommand.indexOf("/to") < fullCommand.indexOf("/from")
-                || fullCommand.length() < 18;
+        validateEventCommand(fullCommand);
+        //Extract out the description, start, end from command.
+        EventFields fields = extractEventFields(fullCommand);
+        validateDuplicate(tasks, fields.description);
+        LocalDateTime startDateTime = parseDateTime(fields.start);
+        LocalDateTime endDateTime = parseDateTime(fields.end);
+        return new Event(fields.description, startDateTime, endDateTime);
+    }
+
+    private EventFields extractEventFields(String fullCommand) throws KuroException {
+        String description = fullCommand.substring(6, fullCommand.indexOf(EVENT_START_INDICATOR)).trim();
+        String start = fullCommand.substring(fullCommand.indexOf(EVENT_START_INDICATOR) + 6,
+                fullCommand.indexOf(EVENT_END_INDICATOR)).trim();
+        String end = fullCommand.substring(fullCommand.indexOf(EVENT_END_INDICATOR) + 4).trim();
+        if (description.isEmpty() || start.isEmpty() || end.isEmpty()) {
+            throw new KuroException(Messages.MISSING_TASK_DESCRIPTION);
+        }
+        return new EventFields(description, start, end);
+    }
+    private void validateEventCommand(String fullCommand) throws KuroException {
+        boolean isInvalidEventCommand = !fullCommand.contains(EVENT_START_INDICATOR)
+                || !fullCommand.contains(EVENT_END_INDICATOR)
+                || fullCommand.indexOf(EVENT_END_INDICATOR) < fullCommand.indexOf(EVENT_START_INDICATOR)
+                || fullCommand.length() <= MINIMUM_EVENT_LENGTH;
         if (isInvalidEventCommand) {
             throw new KuroException(Messages.INVALID_COMMAND);
         }
-        String description = fullCommand.substring(6, fullCommand.indexOf("/from")).trim();
-        String start = fullCommand.substring(fullCommand.indexOf("/from") + 6, fullCommand.indexOf("/to")).trim();
-        String end = fullCommand.substring(fullCommand.indexOf("/to") + 4).trim();
+    }
+    //a class to contain all the fields necessary for event task
+    private static class EventFields {
+        private final String description;
+        private final String start;
+        private final String end;
 
+        EventFields(String description, String start, String end) {
+            this.description = description;
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    // -------------------------------- COMMON HELPER ---------------------------------------------------------------
+    private void validateDuplicate(TaskList tasks, String description) throws KuroException {
         if (tasks.hasDuplicate(description)) {
             throw new KuroException(Messages.DUPLICATE_ERROR);
         }
+    }
+
+    private LocalDateTime parseDateTime(String input) throws KuroException {
         try {
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-            LocalDateTime startDateTime = LocalDateTime.parse(start, formatter);
-            LocalDateTime endDateTime = LocalDateTime.parse(end, formatter);
-            return new Event(description, startDateTime, endDateTime);
+            return LocalDateTime.parse(input, DateTimeFormatter.ofPattern(ParserConstants.DATE_INPUT_PATTERN));
         } catch (DateTimeParseException e) {
             throw new KuroException(Messages.INVALID_DATE);
         }
     }
 
-    //handles Mark, Unmark, Delete, Find commands.
+    // -------------------------------  Mark, Unmark, Delete, Find -------------------------------------------------
     private Task parseTaskCommand(String fullCommand) throws KuroException {
         try {
-            if (fullCommand.split(" ").length < 2) {
+            if (fullCommand.split(" ").length < ParserConstants.MINIMUM_BASIC_TASK_LENGTH) {
                 throw new KuroException(Messages.INVALID_COMMAND);
             }
             return new Task(fullCommand.split(" ")[0]);
@@ -107,5 +166,4 @@ public class CommandParser {
             throw new KuroException(Messages.INVALID_COMMAND);
         }
     }
-
 }
